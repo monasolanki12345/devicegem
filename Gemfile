9e57dae050c7408d1c8c0c045201b34b9289ef12
monasolanki12345 <monasolanki2021@outlook.com>
2022-06-29 14:21:44 +0530

    gemfile,gemfile.lock,application_controller.rb user.rb and index.html.erb files added

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3"
 gem 'devise'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -72,3 +73,5 @@ group :test do
 end
 
 gem 'devise_invitable'
+
+gem "cancancan", "~> 3.4"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable
-
-         
+  belongs_to :role
+        
+  def admin?
+    role.name == 'Admin'
+  end
 end
